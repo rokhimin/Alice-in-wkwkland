0f6acf7a04820d9169d4e7fc6ac7c583d56eb78b
rokhimin wahid <rokhim.whd@gmail.com>
2019-10-16 22:30:05 +0700

    DELETED PLUG_COWBOY

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule AliceInWkwkland.MixProject do
   def project do
     [
       app: :alice_in_wkwkland,
-      version: "0.1.0",
-      elixir: "~> 1.7",
+      version: "1.0.0",
+      elixir: "~> 1.9.2",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -23,7 +23,6 @@ defmodule AliceInWkwkland.MixProject do
       {:poison, "~> 3.0"},
       {:plug, "~> 1.6"},
       {:cowboy, "~> 2.4"},
-      {:plug_cowboy, "~> 2.0"},
       {:credo, "~> 0.10", except: :prod, runtime: false}
     ]
   end
